5ffe6e273009c82d4be2244fd0e7a1479b12526d
paupu <paupu@DESKTOP-C06S9P8>
2023-10-13 10:04:28 +0200

    Primer tareaClase

--- Session3-master/src/test/CuentaTest.java
+++ Session3-master/src/test/CuentaTest.java
@@ -15,10 +15,12 @@ import pkg.Cuenta;
 class CuentaTest {
 	
 	static Cuenta aux;
+	static Cuenta aux1;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		aux=new Cuenta("12345", "Manuel" , 1000);
+		aux=new Cuenta("12345", "Manuel" , 50);
+		aux1=new Cuenta("67890", "Levi" , 0);
 	}
 
 	@AfterAll
@@ -28,6 +30,7 @@ class CuentaTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		aux.setSaldo(0);
+		
 	}
 
 	@AfterEach
@@ -45,6 +48,46 @@ class CuentaTest {
 	void testretirar() throws Exception {
 		aux.retirar(100);
 		assertEquals(aux.getSaldo() ,0 );
+	}
+	@Test
+	void testreintegro200auxprimer() throws Exception {
+		aux.ingresar(200);
+		assertEquals(aux.getSaldo() ,1000 );
+	}
+	@Test
+	void testreintegro350aux1() throws Exception {
+		aux1.ingresar(350);
+		assertEquals(aux.getSaldo() ,1000 );
+	}
+	@Test
+	void testingresar100aux() throws Exception {
+		aux.ingresar(100);
+		assertEquals(aux.getSaldo() ,1000 );
+	}
+	@Test
+	void testreintegro200aux1() throws Exception {
+		aux1.ingresar(200);
+		assertEquals(aux.getSaldo() ,1000 );
+	}
+	@Test
+	void testreintegro150aux1() throws Exception {
+		aux.ingresar(1000);
+		assertEquals(aux.getSaldo() ,1000 );
+	}
+	@Test
+	void testreintegro200aux() throws Exception {
+		aux.ingresar(1000);
+		assertEquals(aux.getSaldo() ,1000 );
+	}
+	@Test
+	void testingreso50aux1() throws Exception {
+		aux.ingresar(1000);
+		assertEquals(aux.getSaldo() ,1000 );
+	}
+	@Test
+	void testreintegro100aux1() throws Exception {
+		aux.ingresar(1000);
+		assertEquals(aux.getSaldo() ,1000 );
 	}
 
 }
